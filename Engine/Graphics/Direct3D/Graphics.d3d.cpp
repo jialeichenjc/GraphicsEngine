425f8b5136931d55e4c20456cd324f450fa45001
joycechendev <superbrainchen@gmail.com>
2017-09-06 23:28:29 -0600

    Try to fix shader not found bug

--- Engine/Graphics/Direct3D/Graphics.d3d.cpp
+++ Engine/Graphics/Direct3D/Graphics.d3d.cpp
@@ -574,44 +574,12 @@ namespace
 		return result;
 	}
 
-	/*
-	eae6320::cResult InitializeShadingData()
-	{
-		auto result = eae6320::Results::Success;
-
-		if ( !( result = eae6320::Graphics::cShader::s_manager.Load( "data/Shaders/Vertex/example.shd",
-			s_vertexShader, eae6320::Graphics::ShaderTypes::Vertex ) ) )
-		{
-			EAE6320_ASSERT( false );
-			goto OnExit;
-		}
-		if ( !( result = eae6320::Graphics::cShader::s_manager.Load( "data/Shaders/Fragment/example.shd",
-			s_fragmentShader, eae6320::Graphics::ShaderTypes::Fragment ) ) )
-		{
-			EAE6320_ASSERT( false );
-			goto OnExit;
-		}
-		{
-			constexpr uint8_t defaultRenderState = 0;
-			if ( !( result = s_renderState.Initialize( defaultRenderState ) ) )
-			{
-				EAE6320_ASSERT( false );
-				goto OnExit;
-			}
-		}
-
-	OnExit:
-
-		return result;
-	}
-
-	*/
-
+	
 	eae6320::cResult InitializeShadingData()
 	{
 		return s_effectManager.InitializeEffect();
 	}
-
+	
 	eae6320::cResult InitializeViews( const unsigned int i_resolutionWidth, const unsigned int i_resolutionHeight )
 	{
 		auto result = eae6320::Results::Success;
