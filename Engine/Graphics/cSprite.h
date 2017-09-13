4667d87df0e665329ec66f5d40c8823769834a9c
Joyce Chen <superbrainchen@gmail.com>
2017-09-13 16:09:07 -0600

    Replace sprite related data with sprite class

---
+++ Engine/Graphics/cSprite.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "cRenderState.h"
+#include "cSamplerState.h"
+#include "cShader.h"
+#include "sContext.h"
+#include "VertexFormats.h"
+
+#if defined( EAE6320_PLATFORM_GL )
+#include "OpenGL\Includes.h"
+#endif
+
+#if defined(EAE6320_PLATFORM_WINDOWS)
+#include "Direct3D\Includes.h" 
+#endif
+
+#include <Engine/Asserts/Asserts.h>
+#include <Engine/Concurrency/cEvent.h>
+#include <Engine/Logging/Logging.h>
+#include <Engine/Platform/Platform.h>
+#include <Engine/Time/Time.h>
+#include <Engine/UserOutput/UserOutput.h>
+#include <utility>
+
+class cSprite {
+public:
+#if defined( EAE6320_PLATFORM_GL )
+	// A vertex buffer holds the data for each vertex
+	GLuint s_vertexBufferId = 0;
+	// A vertex array encapsulates the vertex data as well as the vertex input layout
+	GLuint s_vertexArrayId = 0;
+#endif
+
+#if defined( EAE6320_PLATFORM_D3D )
+	// A vertex buffer holds the data for each vertex
+	ID3D11Buffer* s_vertexBuffer = nullptr;
+	// D3D has an "input layout" object that associates the layout of the vertex format struct
+	// with the input from a vertex shader
+	ID3D11InputLayout* s_vertexInputLayout = nullptr;
+#endif
+
+	eae6320::cResult Initialize();
+
+	void Draw();
+
+	eae6320::cResult CleanUp();
+
+};
