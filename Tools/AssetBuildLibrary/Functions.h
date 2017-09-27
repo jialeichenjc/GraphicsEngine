bc520992d232fcb4f032c6ec939756abb86e8b7e
joycechendev <superbrainchen@gmail.com>
2017-09-27 10:56:00 -0600

    Update Functions.cpp and Functions.h

--- Tools/AssetBuildLibrary/Functions.h
+++ Tools/AssetBuildLibrary/Functions.h
@@ -1,5 +1,5 @@
 /*
-	This file contains functions used by asset build tools
+This file contains functions used by asset build tools
 */
 
 #ifndef EAE6320_ASSETBUILD_FUNCTIONS_H
@@ -18,31 +18,33 @@ namespace eae6320
 {
 	namespace Assets
 	{
+		eae6320::cResult BuildAssets();
+
 		// Error / Warning Output
 		//-----------------------
 
 		// These functions output asset build errors or warnings in the best way for the platform's build tools
 		// (e.g. for Visual Studio the messages are formatted so that they'll show up in the Error List window)
 
-		void OutputErrorMessage( const char* const i_errorMessage, ... );
-		void OutputErrorMessageWithFileInfo( const char* const i_filePath,
-			const char* const i_errorMessage, ... );
-		void OutputErrorMessageWithFileInfo( const char* const i_filePath,
+		void OutputErrorMessage(const char* const i_errorMessage, ...);
+		void OutputErrorMessageWithFileInfo(const char* const i_filePath,
+			const char* const i_errorMessage, ...);
+		void OutputErrorMessageWithFileInfo(const char* const i_filePath,
 			const unsigned int i_lineNumber,
-			const char* const i_errorMessage, ... );
-		void OutputErrorMessageWithFileInfo( const char* const i_filePath,
+			const char* const i_errorMessage, ...);
+		void OutputErrorMessageWithFileInfo(const char* const i_filePath,
 			const unsigned int i_lineNumber, const unsigned int i_columnNumber,
-			const char* const i_errorMessage, ... );
+			const char* const i_errorMessage, ...);
 
-		void OutputWarningMessage( const char* const i_warningMessage, ... );
-		void OutputWarningMessageWithFileInfo( const char* const i_filePath,
-			const char* const i_warningMessage, ... );
-		void OutputWarningMessageWithFileInfo( const char* const i_filePath,
+		void OutputWarningMessage(const char* const i_warningMessage, ...);
+		void OutputWarningMessageWithFileInfo(const char* const i_filePath,
+			const char* const i_warningMessage, ...);
+		void OutputWarningMessageWithFileInfo(const char* const i_filePath,
 			const unsigned int i_lineNumber,
-			const char* const i_warningMessage, ... );
-		void OutputWarningMessageWithFileInfo( const char* const i_filePath,
+			const char* const i_warningMessage, ...);
+		void OutputWarningMessageWithFileInfo(const char* const i_filePath,
 			const unsigned int i_lineNumber, const unsigned int i_columnNumber,
-			const char* const i_warningMessage, ... );
+			const char* const i_warningMessage, ...);
 	}
 }
 
