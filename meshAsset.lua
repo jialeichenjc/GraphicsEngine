533455564e88ce362050332978136f511572a8d2
Joyce Chen <superbrainchen@gmail.com>
2017-11-05 17:24:19 -0700

    Add mesh lua file

---
+++ meshAsset.lua
@@ -0,0 +1,23 @@
+return
+{
+	-- This table contains a hypothetical list of texture paths
+	-- (using an array)
+	textures =
+	{
+		"somePath",
+		"someOtherPath",
+	},
+	-- This table contains a hypothetical list of shader parameters
+	-- (using a dictionary)
+	parameters =
+	{
+		position_x = 0.0,
+		position_y = 0.0,
+		position_z = 0.0,
+		-- for color reddd
+		r = 1.0,
+		g = 0.0,
+		b = 0.0,
+		a = 1.0
+	},
+}
